529c357ecd8f19c1be57ab8897139a16aa11a92f
Manikandan <mail2dinfo@gmail.com>
2023-02-12 10:35:48 +0000

    rechange

--- src/App.js
+++ src/App.js
@@ -9,7 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <H6>Manikandan</H6>
+        <h6> Mnaikandan</h6>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
